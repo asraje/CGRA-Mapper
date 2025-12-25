b17134c6e82a63f204ca05c500f90972c3a50d46
Arjun-Raje <66715184+Arjun-Raje@users.noreply.github.com>
2025-12-24 16:29:52 -0800

    Upgrade to LLVM 21 and fix CI build

--- test/multicycle/compile.sh
+++ test/multicycle/compile.sh
@@ -1,2 +1,2 @@
 clang-21 -emit-llvm -O3 -fno-unroll-loops -fno-vectorize -o multicycle_test.bc -c multicycle_test.cpp
-opt-21 --loop-unroll --unroll-count=4 multicycle_test.bc -o multicycle_test.bc
+opt-21 -passes="loop-unroll" -unroll-count=4 multicycle_test.bc -o multicycle_test.bc
